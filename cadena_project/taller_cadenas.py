e2c16bae6a8f1110a99b7a0e2c807c5ed956f465
Sofia <dsofiaimbachi@gmail.com>
2024-09-19 17:29:31 -0500

    primer punto del taller

---
+++ cadena_project/taller_cadenas.py
@@ -0,0 +1,7 @@
+# Definimos las variables
+name = "Luis"
+age = 27
+favouriteFood = "Pastas con salsa Boloñesa"
+
+presentation = f"Hola! Mi nombre es  {name}. Yo tengo {age} años, y mi comida favorita es {favouriteFood}."
+print(presentation)
